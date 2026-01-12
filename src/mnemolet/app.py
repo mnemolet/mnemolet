5cb36f4bcabb9b34a7beee2aa5af7bf8f70bb95b
riccione <28346232+riccione@users.noreply.github.com>
2026-01-12 12:11:20 +0700

    refactor(ui): move js code from template to static/chat.js

--- src/mnemolet/app.py
+++ src/mnemolet/app.py
@@ -1,9 +1,20 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from mnemolet.api.app import api_router
 from mnemolet.ui.routes import ui_router
 
 app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent
+
+app.mount(
+    "/static",
+    StaticFiles(directory=BASE_DIR / "ui" / "static"),
+    name="static",
+)
 
 # API
 app.include_router(api_router, prefix="/api")
